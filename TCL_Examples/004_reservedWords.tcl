babbc880c77c32de525cea3b13db02aa9cfb065e
bhaveshtanwani001 <bhaveshtanwani001@hotmail.com>
2018-04-30 14:10:33 +0530

    Update 004_reservedWords.tcl

--- TCL_Examples/004_reservedWords.tcl
+++ TCL_Examples/004_reservedWords.tcl
@@ -9,29 +9,30 @@ if {0} {
 ################################################################################################
 ################################################################################################
 
-after				append				array					auto_execok
-auto_import			auto_load			auto_load_index			auto_qualify
-binary				Bgerror				break					catch
-cd					Clock				close					concat
-continue			Dde					default					else
-elseif				Encoding			eof						error
-eval				Exec				exit					expr
-fblocked			Fconfigure			fcopy					file
-fileevent			Flush				for						foreach
-format				Gets				glob					global
-history				If					info					interp
-join				Lappend				lindex					linsert
-list				Llength				load					lrange
-lreplace			Lsearch				lsort					namespace
-open				Package				pid						pkg_mkIndex
-proc				Puts				pwd						read
-regexp				Regsub				rename					resource
-return				Scan				seek					set
-socket				Source				split					string
-subst				Switch				tclLog					tell
-time				Trace				unknown					unset
-update				Uplevel				upvar					variable
-vwait				While
+after				  append				array					    auto_execok
+auto_import		auto_load			auto_load_index		auto_qualify
+binary				Bgerror				break					    catch
+cd					  Clock				  close					    concat
+continue			Dde					  default					  else
+elseif				Encoding			eof						    error
+eval				  Exec				  exit					    expr
+fblocked			Fconfigure		fcopy					    file
+fileevent			Flush				  for						    foreach
+format				Gets				  glob					    global
+history				If					  info					    interp
+join				  Lappend				lindex					  linsert
+list				  Llength				load					    lrange
+lreplace			Lsearch				lsort					    namespace
+open				  Package				pid						    pkg_mkIndex
+proc				  Puts				  pwd						    read
+regexp				Regsub				rename				    resource
+return				Scan				  seek					    set
+socket				Source				split					    string
+subst				  Switch				tclLog				    tell
+time				  Trace				  unknown				    unset
+update				Uplevel				upvar					    variable
+vwait				  While
+
 }
 
 puts "This file contains the reserved words in TCL."
