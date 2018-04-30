e6b6b0ddc7d3ecb8101ef10b5828cd58436a8f55
bhaveshtanwani001 <bhaveshtanwani001@hotmail.com>
2018-04-30 14:06:04 +0530

    Add files via upload

---
+++ TCL_Examples/004_reservedWords.tcl
@@ -0,0 +1,37 @@
+#!/usr/bin/tclsh
+
+if {0} {
+################################################################################################
+################################################################################################
+#######################################Reserved Words###########################################
+################The following list shows a few of the reserved words in Tcl.####################
+###These reserved words may not be used as constant or variable or any other identifier names.##
+################################################################################################
+################################################################################################
+
+after				append				array					auto_execok
+auto_import			auto_load			auto_load_index			auto_qualify
+binary				Bgerror				break					catch
+cd					Clock				close					concat
+continue			Dde					default					else
+elseif				Encoding			eof						error
+eval				Exec				exit					expr
+fblocked			Fconfigure			fcopy					file
+fileevent			Flush				for						foreach
+format				Gets				glob					global
+history				If					info					interp
+join				Lappend				lindex					linsert
+list				Llength				load					lrange
+lreplace			Lsearch				lsort					namespace
+open				Package				pid						pkg_mkIndex
+proc				Puts				pwd						read
+regexp				Regsub				rename					resource
+return				Scan				seek					set
+socket				Source				split					string
+subst				Switch				tclLog					tell
+time				Trace				unknown					unset
+update				Uplevel				upvar					variable
+vwait				While
+}
+
+puts "This file contains the reserved words in TCL."
